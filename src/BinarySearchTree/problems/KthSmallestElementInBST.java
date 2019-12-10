a82da2eb5c50deb717c8d688c117ca570ff9e11a
Atul Joshi <atjoshi.net@gmail.com>
2019-12-10 15:30:03 +0530

    kth smallest term

--- src/BinarySearchTree/problems/KthSmallestElementInBST.java
+++ src/BinarySearchTree/problems/KthSmallestElementInBST.java
@@ -1,5 +1,25 @@
 package BinarySearchTree.problems;
 
-public class KthSmallestElementInBST {
+import BinaryTree.BinaryTreeNode;
 
+public class KthSmallestElementInBST {
+    public void FindKthSmallestElementInBST(BinaryTreeNode node, int key) {
+
+        int count = 0;
+        FindKthSmallestElementInBSTUtil(node, key, count);
+
+    }
+
+    private BinaryTreeNode FindKthSmallestElementInBSTUtil(BinaryTreeNode node, int kth, int count) {
+
+        if (node == null) {
+            return null;
+        }
+        BinaryTreeNode leftNode = FindKthSmallestElementInBSTUtil(node.getLeft(), kth, count);
+        if (++count == kth) {
+            return node;
+        }
+
+        return null;
+    }
 }
