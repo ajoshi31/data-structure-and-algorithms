ef87345311fd47aeea5508163fb3fbb45c18e218
Atul Joshi <atjoshi.net@gmail.com>
2019-12-17 12:59:42 +0530

    maximum recvtangle

--- src/TuringAlgoTest/MaxSizeTableInOffice.java
+++ src/TuringAlgoTest/MaxSizeTableInOffice.java
@@ -1,4 +1,41 @@
 package TuringAlgoTest;
 
+import DynamicProblems.MaxRectangleInHistogram;
+
+
 public class MaxSizeTableInOffice {
+
+
+    public static void main(String args[]) {
+        int R = 4;
+        int C = 4;
+
+        int A[][] = {
+                {1, 1, 1, 1},
+                {1, 1, 1, 1},
+                {1, 1, 1, 1},
+                {1, 1, 1, 1},
+        };
+        System.out.print("Area of maximum rectangle is " +
+                maxRectangle(R, C, A));
+
+    }
+
+    private static int maxRectangle(int R, int C, int A[][]) {
+
+        int result = MaxRectangleInHistogram.maxRectArea(A[0]);
+
+        for (int i = 1; i < C; i++) {
+
+            for (int j = 0; j < R; j++) {
+                if (A[i][j] == 1) {
+                    A[i][j] = A[i][j] + A[i - 1][j];
+                }
+                result = Math.max(result, MaxRectangleInHistogram.maxRectArea(A[i]));
+            }
+
+        }
+
+        return result;
+    }
 }
