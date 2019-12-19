d76d897823e16f184c5fb42f56d7bbd55f9e0b43
Atul Joshi <atjoshi.net@gmail.com>
2019-12-19 11:10:35 +0530

    shell sort

--- src/Sorting/ShellSort.java
+++ src/Sorting/ShellSort.java
@@ -1,4 +1,21 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class ShellSort {
+
+    public void shellSort(int[] arr, int n) {
+        System.out.println(Arrays.toString(arr));
+        for (int gap = n / 2; gap > 0; gap = gap / 2) {
+            for (int i = gap; i < n; i++) {
+                int temp = arr[i];
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j = j - gap) {
+                    arr[j] = arr[j - gap];
+                }
+                arr[j] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
--- src/Sorting/SortMain.java
+++ src/Sorting/SortMain.java
@@ -4,7 +4,7 @@ public class SortMain {
 
     public static void main(String args[]) {
 
-        int[] input = {3, 4, 1, 6, 18, 5, 10, 11, 2};
+        int[] input = {3, 4, 1, 6, 18, 5, 10, 11, 2, 30, 29, 83, 726, 134, 672, 903, 24, 63, 82, 94, 223, 445, 667};
 
 
 //        new SelectionSort().selectionSort(input, input.length);
@@ -12,7 +12,8 @@ public class SortMain {
 //        new InsertionSort().insertionSort(input, input.length);
 //        new InsertionSort().insertionSortRecursion(input, input.length);
 //        new MergeSort().mergeSort(input, input.length);
-        new QuickSort().quickSort(input, input.length);
+//        new QuickSort().quickSort(input, input.length);
+        new ShellSort().shellSort(input, input.length);
 
     }
 }
