33f3b352750675c10829a5561babab38249f7b0a
Atul Joshi <atjoshi.net@gmail.com>
2020-07-29 04:03:58 +0530

    Updtaed the assignemnt for todo app using ngrx

---
+++ src/CompanyInterview/Kloud/io/assignment.md
@@ -0,0 +1,11 @@
+##TODOs App
+
+1. Create a fake json api server in NodeJS which serves an array of todo objects ( you can get sample from here https://jsonplaceholder.typicode.com/todos)
+
+2. Create an Angular/React/Vue/Svelte app which can display the TODOS and let the users create, edit and update todos. Use state management libraries like NgRx.
+
+
+
+SOLUTION
+
+* [GITHUB LINK](https://github.com/ajoshi31/ngrx-todo-seed) 
--- src/Searching/SumEqualToKInArray.java
+++ src/Searching/SumEqualToKInArray.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class SumEqualToKInArray {
     public static void main(String args[]) {
-        int[] array = {5, 15, 10, 35, 20};
+        int[] array = {5, -15, 10, 35, 20};
         int sum = 45;
         HashMap<Integer, Integer> map = new HashMap<>();
         int x = 0, y = 0;
@@ -12,14 +12,12 @@ public class SumEqualToKInArray {
             System.out.println("There is no such two numbers");
         } else {
             for (int i = 0; i < array.length; i++) {
-
                 if (map.containsKey(array[i])) {
                     x = array[map.get(array[i])];
                     y = array[i];
                 } else {
                     map.put(sum - array[i], i);
                 }
-
             }
         }
         System.out.println(" X :" + x + ", Y: " + y);
